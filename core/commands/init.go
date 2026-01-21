f7172107cbe45a3144b3f4379ca9da8eb7551438
temp <temp3al@proton.me>
2026-01-21 01:15:00 -0600

    cmd handling tweaks

--- core/commands/init.go
+++ core/commands/init.go
@@ -12,9 +12,6 @@ var (
 	ENABLE_CHAT_COMMANDS  bool = true
 	ENABLE_SLASH_COMMANDS bool = true
 	// Unregister all slash commands out on exit.
-	//
-	// NOTE: Bots are rate limited to making 200 app commands per day, per guild.
-	// Don't use on a big command list unless you need to remove them all, or for cache reasons.
 	CLEAR_SLASH_ON_EXIT bool = false
 	ChatPrefix               = []string{"+"}
 )
@@ -35,7 +32,8 @@ func Register(cmd CommandEntry) {
 			cmd_name,
 		)
 	}
-	command_map[cmd_name] = &cmd // register our CommandEntry type
+	// register our CommandEntry type
+	command_map[cmd_name] = &cmd
 }
 
 // Return our list of registered commands.
@@ -53,7 +51,7 @@ func InitCommands() {
 		return
 	}
 	if ENABLE_SLASH_COMMANDS {
-		log.Println("Registering slash commands...")
+		log.Println("registering slash commands...")
 		for i, ecmd := range command_map {
 			cmd, err := s.ApplicationCommandCreate(s.State.User.ID, "", &ecmd.AppCommand)
 			if err != nil {
@@ -62,23 +60,20 @@ func InitCommands() {
 			regcmd_map[i] = cmd
 		}
 		s.AddHandler(handleCommandViaSlash)
-		log.Print("Slash command registration successful!")
+		log.Print("slash command registration successful!")
 	}
 	if ENABLE_CHAT_COMMANDS {
 		s.AddHandler(handleCommandViaChat)
-		log.Println("Listening for commands in chat!")
+		log.Println("listening for commands in chat!")
 	}
 }
 
 // Unregister all slash commands out of our bot.
-//
-// NOTE: Bots are rate limited to making 200 app commands per day, per guild.
-// Don't use on a big command list unless you need to remove them all, or for cache reasons.
 func ClearSlashCommands() {
 	s := globals.Session
 
 	if CLEAR_SLASH_ON_EXIT {
-		log.Println("Removing commands...")
+		log.Println("removing commands...")
 		for _, v := range regcmd_map {
 			err := s.ApplicationCommandDelete(s.State.User.ID, "", v.ID)
 			if err != nil {
